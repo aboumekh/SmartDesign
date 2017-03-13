9e8d5fa11b6134d3dcf01de367ecdd2a45c241c4
aboumekh <aboubakr.mekhatria@gmail.com>
2017-03-13 14:41:49 -0400

    Update main.js

--- WEB-Recorder/js/main.js
+++ WEB-Recorder/js/main.js
@@ -82,24 +82,6 @@ function toggleRecording( e ) {
     }
 }
 
-function Recordtimer(audioRecorder){
-				
-			if (e.classList.contains("recording")) {
-			//to record time
-			var Start_time = new Date();
-
-			}
-			else {
-			//To stop recording
-		    var end_time = new Date();
-			var recordingtime = end_time - Start_time;
-			var seconds = Math.round(Start_time/1000);
-			var minutes = Math.round(seconds/60);
-			var hours = Math.round(minutes /60);
-			document.getElementById("Recordingtime").innerHTML= (hours":"minutes":"seconds);
-			}
-}
-
 function convertToMono( input ) {
     var splitter = audioContext.createChannelSplitter(2);
     var merger = audioContext.createChannelMerger(2);
