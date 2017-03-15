686d0a3eee7dde6c5253f2d01e759c010e4767c7
aboumekh <aboubakr.mekhatria@gmail.com>
2017-03-14 21:15:42 -0400

    Update main.js

--- WEB-Recorder/js/main.js
+++ WEB-Recorder/js/main.js
@@ -24,7 +24,7 @@ var audioInput = null,
 var seconds = 1, 
     minutes = 1,
     hours = 1;
-window.onload = function TimerDisplay() {
+function TimerDisplay() {
 	seconds++;
 	if (seconds == 60){ 
 	    seconds =0;
