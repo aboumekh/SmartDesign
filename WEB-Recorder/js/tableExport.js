1e84714802ebabcbc978da297efbbbadc65a9592
aboumekh <aboubakr.mekhatria@gmail.com>
2017-04-15 18:46:53 -0400

    Update tableExport.js

--- WEB-Recorder/js/tableExport.js
+++ WEB-Recorder/js/tableExport.js
@@ -84,8 +84,7 @@ THE SOFTWARE.*/
 					doc.setFontSize(pdfFontSize);
 					doc.text(20,280, 'Powered by Thrush');						
 					// Output as Data URI
-					 // 
-				        doc.output('datauriNew'); // to open the pdf in a new window.*/
+					 // doc.output('datauriNew'); // to open the pdf in a new window.*/
 				    doc.output('datauri');
 				    	
 				}
