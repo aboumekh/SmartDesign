82c5e6a8b39b51cae3ccab7eb2a63809679cc49d
aboumekh <aboubakr.mekhatria@gmail.com>
2017-04-16 20:06:20 -0400

    new windows
    add print pdf in new window

--- WEB-Recorder/js/jspdf/jspdf.js
+++ WEB-Recorder/js/jspdf/jspdf.js
@@ -299,6 +299,9 @@ var jsPDF = function(){
 			if(type == 'datauri') {
 				document.location.href = 'data:application/pdf;base64,' + Base64.encode(buffer);
 			}
+			if(type == 'datauriNew') {   
+   			 window.open('data:application/pdf;base64,' + Base64.encode(buffer));
+			}
 			// @TODO: Add different output options
 		},
 		setFontSize: function(size) {
